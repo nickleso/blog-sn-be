5e0a8534f9a2730e2e4dbde178e831438afcab34
Nick Leso <nickleso.work@gmail.com>
2024-11-08 22:31:06 +0200

    Update get-posts-param.dto.ts

--- src/posts/dtos/get-posts-param.dto.ts
+++ src/posts/dtos/get-posts-param.dto.ts
@@ -5,7 +5,7 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 export class GetPostsParamDto {
   @ApiPropertyOptional({
     description: 'Get post with a specific id',
-    example: '672d171d0904b0427a878c2f',
+    example: '672e6dd8134acc8c0c1464ae',
   })
   @IsOptional()
   @IsMongoId()
